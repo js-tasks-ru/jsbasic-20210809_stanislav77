df9197eed6af39c9b2c1db88252469b427de2d9b
s.solovyev <s.solovyev@rlt.ru>
2021-08-27 10:10:49 +0300

    3-module 4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+    let resultStr = "";
+    let resultArr = users.filter(item => item.age <= age);
+    for (let i = 0; i < resultArr.length; i++)
+    {
+        resultArr[i].strNameAndBalance= resultArr[i].name + ", " + resultArr[i].balance + "\n";
+        resultStr += resultArr[i].strNameAndBalance;
+    };
+    return resultStr.substr(0, resultStr.length-1);
 }
